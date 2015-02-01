dd4b565f8e1ee588337bd9733f05738856adbd06
Patrick Servello <patrick.servello@gmail.com>
2015-02-01 14:06:28 +0000

    Update example.c

--- src/example.c
+++ src/example.c
@@ -3,24 +3,14 @@
 #include <stdint.h>
 #include "usart_printf/usart_printf.h"
 
-#define FAULT_VCC_SHORT 3
-#define FAULT_GND_SHORT 2
-#define FAULT_OPEN_CONN 1
-#define NO_FAULT 		0
-
 int main(void) {
-
-	usart_initialize();
+    usart_initialize();
 	stdout = &uart_output;
 	_delay_ms(10);
-	
 	for(;;) {
-
 		printf("Hello AVR printf");
 		_delay_ms(1000);
 	}
-
-
 }
 
 
