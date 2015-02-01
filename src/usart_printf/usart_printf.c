b0b3d81a73ed2095e5b47964b6695abd2edfa419
Patrick Servello <patrick.servello@gmail.com>
2015-02-01 14:08:29 +0000

    Update usart_printf.c

--- src/usart_printf/usart_printf.c
+++ src/usart_printf/usart_printf.c
@@ -2,17 +2,13 @@
 #include <avr/sfr_defs.h>
 #include <stdio.h>
 
-#define BAUD_RATE 9600
+#define BAUD_RATE      9600
 #define BAUD_PRESCALE (F_CPU/16/BAUD_RATE-1)
 
 void usart_initialize(void) {
 	UBRR0H = (BAUD_PRESCALE >> 8);
 	UBRR0L = BAUD_PRESCALE;
-
-	//Enable receive and transmit
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-
-	// Set frame to 8 bits
 	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);
 }
 
@@ -22,9 +18,7 @@ void usart_send_byte(char byte, FILE *stream) {
 	}
 		loop_until_bit_is_set(UCSR0A,UDRE0);
 		UDR0 = byte;
-	
 }
-
 
 char usart_get_byte(FILE *stream) {
     loop_until_bit_is_set(UCSR0A, RXC0);
